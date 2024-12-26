9f3c31e60318aa9d2d4ef3197e894699745dd0d5
an ma <tommycveigh@gmail.com>
2024-12-26 05:56:52 +0300

    lab2

---
+++ src/lab2/Calculator.java
@@ -0,0 +1,40 @@
+package lab2;
+
+public class Calculator {
+    public static int add(int arg1, int arg2) {
+        return arg1 + arg2 ;
+    }
+    public static long add(long arg1, long arg2) {
+        return arg1 + arg2 ;
+    }
+    public static double add(double arg1, double arg2) {
+        return arg1 + arg2 ;
+    }
+    public static int multiply(int arg1, int arg2) {
+        return arg1 * arg2 ;
+    }
+    public static long multiply(long arg1, long arg2) {
+        return arg1 * arg2 ;
+    }
+    public static double multiply(double arg1, double arg2) {
+        return arg1 * arg2 ;
+    }
+    public static int divide(int arg1, int arg2) {
+        return arg1 / arg2 ;
+    }
+    public static long divide(long arg1, long arg2) {
+        return arg1 / arg2 ;
+    }
+    public static double divide(double arg1, double arg2) {
+        return arg1 / arg2 ;
+    }
+    public static int substract(int arg1, int arg2) {
+        return arg1 - arg2 ;
+    }
+    public static long substract(long arg1, long arg2) {
+        return arg1 - arg2 ;
+    }
+    public static double substract(double arg1, double arg2) {
+        return arg1 - arg2 ;
+    }
+}
---
+++ src/lab2/Planet.java
@@ -0,0 +1,14 @@
+package lab2;
+
+public class Planet {
+    private String name;
+    private long population;
+    private double mass;
+    public Planet() {
+    }
+    public Planet( String name, long population, double mass ) {
+        this.name = name;
+        this.population = population;
+        this.mass =mass;
+    }
+}
